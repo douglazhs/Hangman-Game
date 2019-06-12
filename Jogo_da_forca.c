01b6b9e32aa038f90b4340f4899229712201d184
douglazhs <douglinhas311@gmail.com>
2019-06-12 19:42:53 -0300

    Updates

--- Jogo_da_forca.c
+++ Jogo_da_forca.c
@@ -62,52 +62,54 @@ void jogar () {
 	char dica[50];
 	char letra;
 	char resp;
+	char esconde[] = "_";
 	int contadorErros = 0, i, contador = 0;
 	int escolha;
+	int chances = 6;
 
 	do {
 
 		system ("cls");
-		
-		
+
+
 		printf ("\n\t\t\t\t\t   Informe a a palavra para começar.\n");
-		
+
 		do {
 			printf ("\n\t\t\t\t\t\t      PALAVRA: ");
 			gets (palavra);
-			
+
 			system ("cls");
 
 			if (strlen (palavra) == 0) {
 				printf ("\t\t\t\tPor favor, digite uma palavra para começar o jogo!");
 			}
-	
+
 		} while (strlen (palavra) == 0);
-	
+
 		for (i = 0; i < strlen (palavra); i++) {
 			palavra[i] = toupper (palavra[i]);
 		}
 		fflush (stdin);
 
-		
+
 		printf ("\n\t\t\tAgora informe a dica para ficar mais facil de seu amigo conseguir acertar.\n");
 
 		do {
 
 			printf ("\n\t\t\t\t\t\t      DICA: ");
 			gets (dica);
-			
+
 			system ("cls");
-			
+
 			for (i = 0; i < strlen (dica); i++) {
 				dica[i] = toupper (dica[i]);
 			}
 			fflush (stdin);
-			
+
 			if (strlen(dica) == 0) {
 				printf ("\n\t\t\t\t\t   Por favor, digite uma dica!");
 			}
-			
+
 		} while (strlen (dica) == 0);
 
 		system ("cls");
@@ -117,57 +119,73 @@ void jogar () {
 		printf ("\n\t\tR: ");
 		scanf ("%c", &resp);
 		fflush (stdin);
-	
+
 	} while (resp != 's' && strlen (palavra) == 0 && strlen (dica) == 0);
 
 	system ("cls");
-
-
+	
 	do {
-		printf ("\n------------------------------------------------------------------------------------------------------------------------");
-		printf ("\n\t\t\t*Agora, você pode escolher uma letra ou tentar acertar sua palavra!");
-		printf ("\n\t\t\t*Lembrando que você pode errar até 5 vezes.");
-		printf ("\n------------------------------------------------------------------------------------------------------------------------");
-		printf ("\n\t\t\t\t\t\t\tVALENDO!");
+		for (contador = 0; contadorErros < 7; contador++) {
+            for (contador = 0; contador < strlen(palavra); contador++) {
+				printf ("------------------------------------------------------------------------------------------------------------------------");
+				printf ("\n\t\t\tINSTRUÇÕES:");
+				printf ("\n\n\t\t\t*Agora, você pode escolher uma letra ou tentar acertar sua palavra!");
+				printf ("\n\t\t\t*Lembrando que você pode errar até 6 vezes.");
+				printf ("\n\t\t\t*Basta Digitar \"#\" quando souber a palavra e quiser chutar.");
+				printf ("\n------------------------------------------------------------------------------------------------------------------------");
+				printf ("\n\t\t\t\t\t\t\tVALENDO!");
 
-		printf ("\n\n\tDICA: %s", dica);
-		printf ("\n\n\tQuantidade de letras na palavra: %d", strlen (palavra));
-		
-		boneco (contadorErros);
-		
-		do {
-			printf ("_ ");
-			i++;
-		
-		} while (i < strlen (palavra));
-		
-		
-		
-		printf ("\n\n\tLetras já escolhidas: %c", letra);
+				printf ("\n\n\tDICA: %s", dica);
+				printf ("\n\n\tQuantidade de letras na palavra: %d", strlen (palavra));
+				printf ("\n\tChances disponíveis: %d", chances);
 
-		printf ("\n\n\tTentar uma letra: ");
+				boneco (contadorErros);
 
-		do {
-			scanf ("%c", &letra);
-			letra = toupper(letra);
-			fflush (stdin);
-		} while (contadorErros < 5);
+				printf ("%s", esconde);
+			
+				printf ("\n\n\tLetras já escolhidas: %c ", letra);
+				printf ("\n\n\tTentar uma letra: ");
+				scanf ("%c", &letra);
+				letra = toupper(letra);
+				fflush (stdin);
 
+				if (letra == palavra[contador]) {
+					esconde[contador] = letra;
+				} else if (letra == '#') {
+					printf ("\n\tDigite o seu chute: ");
+					gets (chute);
+					fflush (stdin);
+					for (i = 0; i < strlen (chute); i++) {
+						chute[i] = toupper (chute[i]);
+					}
+				} else {
+					contadorErros++;
+					chances--;
+				}
+
+				if (palavra[contador] == ' ') {
+					esconde[contador] = '-';
+				}
+				
+			system ("cls");
+
+			}
+
+		}
+		
 		if (letra == '#') {
 			printf ("\n\tDigite o seu chute: ");
 			gets (chute);
-			
+			fflush (stdin);
+
 			for (i = 0; i < strlen (chute); i++) {
 				chute[i] = toupper (chute[i]);
 			}
-			
-			fflush (stdin);
+
 		}
-		
-		system ("pause");
 
 	} while (letra != '#');
-	
+
 }
 
 void boneco (int contadorErros) {
@@ -186,77 +204,77 @@ void boneco (int contadorErros) {
 			printf ("\n\t|");
 		}
 		else if (contadorErros == 1) {
-			printf ("\n_________________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t_________________");
+			printf ("\n\t|            ***");
+			printf ("\n\t|           *   *");
+			printf ("\n\t|            ***");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		} else if (contadorErros == 2) {
-			printf ("\n _______________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|			 |");
-			printf ("\n|		     |");
-			printf ("\n|	         |");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t_______________");
+			printf ("\n\t|            ***");
+			printf ("\n\t|           *   *");
+			printf ("\n\t|            ***");
+			printf ("\n\t|             |");
+			printf ("\n\t|             |");
+			printf ("\n\t|             |");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		} else if (contadorErros == 3) {
-			printf ("\n _______________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|			 |\\");
-			printf ("\n|		     | \\");
-			printf ("\n|	         |");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t _______________");
+			printf ("\n\t|            ***");
+			printf ("\n\t|           *   *");
+			printf ("\n\t|            ***");
+			printf ("\n\t|             |\\");
+			printf ("\n\t|             | \\");
+			printf ("\n\t|             |");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		} else if (contadorErros == 4) {
-			printf ("\n _______________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|			/|\\");
-			printf ("\n|		   / | \\");
-			printf ("\n|	         |");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t _______________");
+			printf ("\n\t|            ***");
+			printf ("\n\t|           *   *");
+			printf ("\n\t|            ***");
+			printf ("\n\t|            /|\\");
+			printf ("\n\t|           / | \\");
+			printf ("\n\t|             |");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		} else if (contadorErros == 5) {
-			printf ("\n _______________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|			/|\\");
-			printf ("\n|		   / | \\");
-			printf ("\n|	         |");
-			printf ("\n|	          \\");
-			printf ("\n|               \\");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t_______________");
+			printf ("\n\t|             ***");
+			printf ("\n\t|            *   *");
+			printf ("\n\t|             ***");
+			printf ("\n\t|             /|\\");
+			printf ("\n\t|            / | \\");
+			printf ("\n\t|              |");
+			printf ("\n\t|               \\");
+			printf ("\n\t|                \\");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		} else if (contadorErros == 6) {
-			printf ("\n________________");
-			printf ("\n|			***");
-			printf ("\n|		   *	*");
-			printf ("\n|		   	***");
-			printf ("\n|			/|\\");
-			printf ("\n|		   / | \\");
-			printf ("\n|	         |");
-			printf ("\n|	        / \\");
-			printf ("\n|           /   \\");
-			printf ("\n|");
-			printf ("\n|");
+			printf ("\n\t________________");
+			printf ("\n\t|            ***");
+			printf ("\n\t|           *   *");
+			printf ("\n\t|            ***");
+			printf ("\n\t|            /|\\");
+			printf ("\n\t|           / | \\");
+			printf ("\n\t|             |");
+			printf ("\n\t|            / \\");
+			printf ("\n\t|           /   \\");
+			printf ("\n\t|");
+			printf ("\n\t|");
 		}
 }
 
