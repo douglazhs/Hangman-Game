4a76bc5ad31f16e0b096086b9d026e9b2de3312f
douglazhs <douglinhas311@gmail.com>
2019-06-18 19:45:32 -0300

    Update

--- Jogo_da_forca.c
+++ Jogo_da_forca.c
@@ -13,7 +13,8 @@
 #include <ctype.h>
 
 void menu ();
-void jogar ();
+void multiPlayer ();
+void singlePlayer ();
 void boneco (int contadorErros);
 
 int main () {
@@ -40,17 +41,18 @@ void menu () {
 		printf ("\n\t\t\t\t\t\t|             / \\");
 		printf ("\n\t\t\t\t\t\t|            /   \\");
 		printf ("\n========================================================================================================================");
-		printf ("\n\t\t\t\t\t              [1] JOGAR            ");
-		printf ("\n\n\t\t\t\t\t         [Outra tecla] SAIR     ");
+		printf ("\n\t\t\t\t\t           [1] JOGAR MODO MULTIPLAYER");
+		printf ("\n\t\t\t\t\t           [2] JOGAR MODO SINGLE PLAYER");
+		printf ("\n\t\t\t\t\t           [3] SAIR");
 		printf ("\n\t\t\t\t\t");
 
 
-		printf ("\n\n\t\t\t\t\t\tDigite uma das opções: ");
+		printf ("\n\n\t\t\t\t\t\t   OPÇÃO -> ");
 		scanf ("%d", &opcao);
 		fflush (stdin);
 
 		if (opcao == 1) {
-			jogar ();
+			multiPlayer ();
 		} else {
 			break;
 		}
@@ -58,19 +60,20 @@ void menu () {
 	} while (opcao != 1);
 }
 
-void jogar () {
-	char palavra[50], chute[50], dica[50], letra[50], esconde[50] = "_";
+void multiPlayer () {
+	char palavra[50], chute[50], dica[50], letra, mostraPalavra[50];
 	char resp, tecla;
 	int contadorErros = 0, i, contador;
 	int chances = 7, pontos = 0;
 
 	do {
 		system ("cls");
-
-		printf ("\n\tInforme a a palavra para começar.\n");
+		
+		printf ("\tCERTIFIQUE-SE DE QUE SEU AMIGO NÃO O VEJA ESCREVENDO A PALAVRA!");
+		printf ("\n\n\tInforme a a palavra para começar.\n");
 
 		do {
-			printf ("\n\tPALAVRA: ");
+			printf ("\n\tPALAVRA -> ");
 			gets (palavra);
 
 			system ("cls");
@@ -90,7 +93,7 @@ void jogar () {
 
 		do {
 
-			printf ("\n\tDICA: ");
+			printf ("\n\tDICA -> ");
 			gets (dica);
 
 			system ("cls");
@@ -110,197 +113,210 @@ void jogar () {
 
 		printf ("\n\t\t\t\t\t\t\t*ATENÇÃO*");
 		printf ("\n\n\tTem certeza de que deseja escolher a palavra \"%s\" com a dica \"%s\"? <S> SIM <Outra> NAO\n", palavra, dica);
-		printf ("\n\tR: ");
+		printf ("\n\t-> ");
 		scanf ("%c", &resp);
 		fflush (stdin);
 
 	} while (resp != 's');
 
 	system ("cls");
+	
+	for (contador = 0; contador < strlen (palavra); contador++) {
+		mostraPalavra[contador] = '_';
+	}
 
-	for (contador = 0; contadorErros < 8 && contador < strlen(palavra); contador++) {
-		esconde[contador] = '_';
-		//while (chances > 0) {
+	for (contador = 0; contadorErros < 8; contador++) {
+  		printf ("------------------------------------------------------------------------------------------------------------------------");
+		printf ("\n\t\t\tINSTRUÇÕES:");
+		printf ("\n\n\t\t\t*Agora, você pode escolher uma letra ou tentar acertar sua palavra!");
+		printf ("\n\t\t\t*Lembrando que você pode errar até 6 vezes.");
+		printf ("\n\t\t\t*Basta Digitar \"#\" quando souber a palavra e quiser chutar.");
+		printf ("\n------------------------------------------------------------------------------------------------------------------------");
+		printf ("\n\t\t\t\t\t\t\tVALENDO!");
+			
+		printf ("\n\n\tDICA: %s", dica);
+		printf ("\n\n\tQuantidade de letras na palavra: %d", strlen (palavra));
+		printf ("\n\tChances disponíveis: %d", chances);
 
-  			printf ("------------------------------------------------------------------------------------------------------------------------");
-			printf ("\n\t\t\tINSTRUÇÕES:");
-			printf ("\n\n\t\t\t*Agora, você pode escolher uma letra ou tentar acertar sua palavra!");
-			printf ("\n\t\t\t*Lembrando que você pode errar até 6 vezes.");
-			printf ("\n\t\t\t*Basta Digitar \"#\" quando souber a palavra e quiser chutar.");
-			printf ("\n------------------------------------------------------------------------------------------------------------------------");
-			printf ("\n\t\t\t\t\t\t\tVALENDO!");
+		boneco (contadorErros);
+			
+		for (contador = 0; contador < strlen (palavra); contador ++) {
+			printf ("%c ", mostraPalavra[contador]);
 
-			printf ("\n\n\tDICA: %s", dica);
-			printf ("\n\n\tQuantidade de letras na palavra: %d", strlen (palavra));
-			printf ("\n\tChances disponíveis: %d", chances);
+			if (palavra[contador] == ' ') {
+				mostraPalavra[contador] = '-';
+			}
+		}
 
-			boneco (contadorErros);
+		printf ("\n\n\tLetras já escolhidas: %c ", letra);
+		printf ("\n\n\tTentar uma letra -> ");
+		scanf ("%c", &letra);
+		letra = toupper(letra);
+		fflush (stdin);
+		
+		for (contador = 0; contador < strlen(palavra); contador++) {
 
-			printf ("%s ", esconde);
-
-			printf ("\n\n\tLetras já escolhidas: %c ", letra[contador]);
-			printf ("\n\n\tTentar uma letra: ");
-			scanf ("%c", &letra[contador]);
-			letra[contador] = toupper(letra[contador]);
-			fflush (stdin);
-
-			if (letra[contador] == palavra[contador]) {
-				esconde[contador] = palavra[contador];
+			if (letra == palavra[contador]) {
+				mostraPalavra[contador] = letra;
 				pontos++;
-			} else if (letra[contador] == '#') {
-				printf ("\n\tDigite o seu chute: ");
-				gets (chute);
-				fflush (stdin);
-
-				for (i = 0; i < strlen (chute); i++) {
-					chute[i] = toupper (chute[i]);
-				}
-
-				if (strcmp (chute, palavra) == 0) {
-					printf ("\n\t\t\t\t\t     VOCÊ GANHOU! Palavra: %s", palavra);
-					printf ("\n\n\n\tDigite uma tecla pra voltar ao menu: ");
-					scanf ("%c", &tecla);
-					fflush (stdin);
-					system ("cls");
-					menu ();
-				} else {
-					system ("cls");
-					boneco (contadorErros = 7);
-					printf ("\n\t\t\t\t\t     VOCÊ PERDEU! A palavra era: %s", palavra);
-					printf ("\n\n\n\tDigite uma tecla pra voltar ao menu: ");
-					scanf ("%c", &tecla);
-					fflush (stdin);
-					system ("cls");
-					menu ();
-				}
 			} else {
 				contadorErros++;
-				chances--;
 			}
 
-			if (pontos == strlen (palavra)) {
+		}
+			
+		if (letra == '#') {
+			printf ("\n\tDigite o seu chute -> ");
+			gets (chute);
+			fflush (stdin);
+
+			for (i = 0; i < strlen (chute); i++) {
+				chute[i] = toupper (chute[i]);
+			}
+
+			if (strcmp (chute, palavra) == 0) {
 				printf ("\n\t\t\t\t\t     VOCÊ GANHOU! Palavra: %s", palavra);
-				printf ("\n\n\n\tDigite uma tecla pra voltar ao menu: ");
+				printf ("\n\n\n\tDigite uma tecla pra voltar ao menu ->> ");
 				scanf ("%c", &tecla);
 				fflush (stdin);
 				system ("cls");
 				menu ();
-			}
-
-			if (contadorErros == 7) {
+			} else {
 				system ("cls");
-				boneco (contadorErros);
+				boneco (contadorErros = 7);
 				printf ("\n\t\t\t\t\t     VOCÊ PERDEU! A palavra era: %s", palavra);
-				printf ("\n\n\n\tDigite uma tecla pra voltar ao menu: ");
+				printf ("\n\n\n\tDigite uma tecla pra voltar ao menu ->> ");
 				scanf ("%c", &tecla);
 				fflush (stdin);
 				system ("cls");
 				menu ();
 			}
+		}
+
+		if (pontos == strlen (palavra)) {
+			printf ("\n\t\t\t\t\t     VOCÊ GANHOU! Palavra: %s", palavra);
+			printf ("\n\n\n\tDigite uma tecla pra voltar ao menu ->> ");
+			scanf ("%c", &tecla);
+			fflush (stdin);
 			system ("cls");
-		//}
-	}   
+			menu ();
+		}
+
+		if (contadorErros == 7) {
+			system ("cls");
+			boneco (contadorErros);
+			printf ("\n\t\t\t\t\t     VOCÊ PERDEU! A palavra era: %s", palavra);
+			printf ("\n\n\n\tDigite uma tecla pra voltar ao menu ->> ");
+			scanf ("%c", &tecla);
+			fflush (stdin);
+			system ("cls");
+			menu ();
+		}
+		
+		system ("cls");
+	}
 }
+
 
 void boneco (int contadorErros) {
 
-		if (contadorErros == 0) {
-			printf ("\n\t_________________");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		}
-		else if (contadorErros == 1) {
-			printf ("\n\t_________________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 2) {
-			printf ("\n\t_______________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|             |");
-			printf ("\n\t|             |");
-			printf ("\n\t|             |");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 3) {
-			printf ("\n\t _______________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|             |\\");
-			printf ("\n\t|             | \\");
-			printf ("\n\t|             |");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 4) {
-			printf ("\n\t _______________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|            /|\\");
-			printf ("\n\t|           / | \\");
-			printf ("\n\t|             |");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 5) {
-			printf ("\n\t_______________");
-			printf ("\n\t|             ***");
-			printf ("\n\t|            *   *");
-			printf ("\n\t|             ***");
-			printf ("\n\t|             /|\\");
-			printf ("\n\t|            / | \\");
-			printf ("\n\t|              |");
-			printf ("\n\t|               \\");
-			printf ("\n\t|                \\");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 6) {
-			printf ("\n\t________________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|            /|\\");
-			printf ("\n\t|           / | \\");
-			printf ("\n\t|             |");
-			printf ("\n\t|            / \\");
-			printf ("\n\t|           /   \\");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		} else if (contadorErros == 7) {
-			printf ("\n\t________________");
-			printf ("\n\t|            ***");
-			printf ("\n\t|           *   *");
-			printf ("\n\t|            ***");
-			printf ("\n\t|          -------");
-			printf ("\n\t|            /|\\");
-			printf ("\n\t|           / | \\");
-			printf ("\n\t|             |");
-			printf ("\n\t|            / \\");
-			printf ("\n\t|           /   \\");
-			printf ("\n\t|");
-			printf ("\n\t|");
-		}
+	if (contadorErros == 0) {
+		printf ("\n\t_________________");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	}else if (contadorErros == 1) {
+		printf ("\n\t_________________");
+		printf ("\n\t|            ***");
+		printf ("\n\t|           *   *");
+		printf ("\n\t|            ***");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 2) {
+		printf ("\n\t_______________");
+		printf ("\n\t|           ***");
+		printf ("\n\t|          *   *");
+		printf ("\n\t|           ***");
+		printf ("\n\t|            |");
+		printf ("\n\t|            |");
+		printf ("\n\t|            |");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 3) {
+		printf ("\n\t _______________");
+		printf ("\n\t|            ***");
+		printf ("\n\t|           *   *");
+		printf ("\n\t|            ***");
+		printf ("\n\t|             |\\");
+		printf ("\n\t|             | \\");
+		printf ("\n\t|             |");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 4) {
+		printf ("\n\t _______________");
+		printf ("\n\t|            ***");
+		printf ("\n\t|           *   *");
+		printf ("\n\t|            ***");
+		printf ("\n\t|            /|\\");
+		printf ("\n\t|           / | \\");
+		printf ("\n\t|             |");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 5) {
+		printf ("\n\t_______________");
+		printf ("\n\t|           ***");
+		printf ("\n\t|          *   *");
+		printf ("\n\t|           ***");
+		printf ("\n\t|           /|\\");
+		printf ("\n\t|          / | \\");
+		printf ("\n\t|            |");
+		printf ("\n\t|             \\");
+		printf ("\n\t|              \\");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 6) {
+		printf ("\n\t________________");
+		printf ("\n\t|            ***");
+		printf ("\n\t|           *   *");
+		printf ("\n\t|            ***");
+		printf ("\n\t|            /|\\");
+		printf ("\n\t|           / | \\");
+		printf ("\n\t|             |");
+		printf ("\n\t|            / \\");
+		printf ("\n\t|           /   \\");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	} else if (contadorErros == 7) {
+		printf ("\n\t________________");
+		printf ("\n\t|            ***");
+		printf ("\n\t|           *   *");
+		printf ("\n\t|            ***");
+		printf ("\n\t|          -------");
+		printf ("\n\t|            /|\\");
+		printf ("\n\t|           / | \\");
+		printf ("\n\t|             |");
+		printf ("\n\t|            / \\");
+		printf ("\n\t|           /   \\");
+		printf ("\n\t|");
+		printf ("\n\t|");
+	}
 }
+
